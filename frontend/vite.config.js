d502c5fab6fc032a8f2045cd1a6057e0091ce4f4
Your Name <your.email@example.com>
2024-12-23 19:34:03 +0530

    updated_config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5175,
+  },
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit as needed
+    outDir: 'dist', 
   },
 });
