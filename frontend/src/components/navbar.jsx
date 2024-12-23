31372c8bae8b80268f8febb8502aa2a7b32c3e85
Your Name <your.email@example.com>
2024-12-23 18:01:08 +0530

    prod

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -119,7 +119,7 @@ const Navbar = () => {
                             textDecoration: 'none',
                         }}
                     >
-                        BrandName
+                    MIND-MESH 
                     </Typography>
                     <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                         <Button
